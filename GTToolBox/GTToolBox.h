9cea89e450f4c64e429b76d0f6d19a3a813583e2
WMYeah <wm@wmyeah.com>
2017-11-10 15:39:17 +0800

    Initial Commit

---
+++ GTToolBox/GTToolBox.h
@@ -0,0 +1,19 @@
+//
+//  GTToolBox.h
+//  GTToolBox
+//
+//  Created by WMYeah on 2017/11/10.
+//  Copyright © 2017年 GeenTi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GTToolBox.
+FOUNDATION_EXPORT double GTToolBoxVersionNumber;
+
+//! Project version string for GTToolBox.
+FOUNDATION_EXPORT const unsigned char GTToolBoxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GTToolBox/PublicHeader.h>
+
+
